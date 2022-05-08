74dd5e4e42fb5abd81f680d7e71e322d6939ba27
kubaunold <jakub@unold.pl>
2022-05-08 16:43:24 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+"""
+Application performing creating a schedule for Permutation-FlowShop `F*` (all machines have the same sequence).
+It then calculates makespan for some random permutation of jobs and displays the result on a gantt chart.
+
+Steps:
+1) Read operation times into a Matrix
+2) Select permutation of jobs
+3) Calculate makespan
+4) Display gantt chart
+"""
+
+def main():
+    print("Hello world")
+
+if __name__ == '__main__':
+    main()
