b75fb1a6a1c8555e2204995bcf24dcb7f01ed5e7
Kuba <jakub@unold.pl>
2022-05-09 21:59:46 +0200

    rewriting flowshop so that it takes given permutation into account

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ Steps:
 """
 
 from numpy import Inf, zeros
+from ..flowshop.flow_shop_intro.gantt_fs import crear_y_mostrar_gantt_fs
 
 
 
@@ -32,26 +33,36 @@ class FlowShop:
     
     def __init__(self) -> None:
         self.duration = [
-            [5, 10, 6,  8],
-            [8, 15, 5,  7],
-            [8, 5,  7,  9],
+            [4, 4, 2,  3],
+            [3, 1, 1,  2],
+            [4, 1,  2,  1],
         ]
 
-        self.num_machines = len(self.duration)
-        self.num_jobs = len(self.duration[0])
-        self.time_start = zeros((self.num_machines, self.num_jobs), dtype=int)
-        self.makespan = Inf
-        self.permutation = [0, 1, 2, 3]
-        self.schedule = []
+        self.m = len(self.duration)     # number of machines
+        self.n = len(self.duration[0])  # number of tasks
+        self.time_start = zeros((self.m, self.n), dtype=int)    # matrix with operation start times
+        
+        cmax = Inf                      # makespan
+        pi = [3, 1, 0, 2]               # permutation
+        self.solution = (pi, cmax)      # solution is a tuple of permutation and its makespan
+        
+        print(f"FlowShop object created")
 
-        for m in range(self.num_machines):
-            for n in range(self.num_jobs):
-                print(f"{m=}{n=}")
-                # o = Operation(duration=self.op[m][n], machine_number=m, job_number=n)
-                # self.schedule.append(o)
+    def __repr__(self) -> str:
+        return f"My operations: {self.duration}"
+    
+    def calculate_makespan(self, permutation) -> int:
+        self.permutation = permutation
 
-                    # poprzednik technologiczny - operacja z tego zadania, ale poprzedniej maszyny
-                    # poprzednik kolejnościowy - inne zadanie z tej samej maszyny
+        for m in range(self.m):
+            for n in range(self.n):
+
+                # technological ancestor finish - finish time of an operation that's a tech predecessor
+                # poprzednik technologiczny - operacja z tego zadania, ale poprzedniej maszyny
+                taf = self.technological_ancestor_finish(permutation, m, n)
+                # order ancestor finish - finish time of an operation that's an oredr predecessor
+                # poprzednik kolejnościowy - inne zadanie z tej samej maszyny
+                oaf = order_ancestor_finish()
 
                 if m == 0 and n == 0:
                     ancestor_tech_finish = 0
@@ -69,16 +80,35 @@ class FlowShop:
 
                 self.time_start[m][n] = max(ancestor_tech_finish, ancestor_order_finish)
 
-        print(f"{self.time_start=}")
+        return 100
 
-    def __repr__(self) -> str:
-        return f"My operations: {self.duration}"
+    def technological_ancestor_finish(self, m, n):
+        if m == 0 and n == 0:
+            return 0 + self.duration[m][n]
+        elif m != 0 and n == 0:
+            return self.time_start[]
+            ancestor_tech_finish = self.time_start[m-1][n] + self.duration[m-1][n]
+            ancestor_order_finish = 0
+        elif m == 0 and n != 0:
+            ancestor_tech_finish = 0
+            ancestor_order_finish = self.time_start[m][n-1] + self.duration[m][n-1]
+        elif m != 0 and n != 0:
+            ancestor_tech_finish = self.time_start[m-1][n] + self.duration[m-1][n]
+            ancestor_order_finish = self.time_start[m][n-1] + self.duration[m][n-1]
+
+        return taf
+
+
+    def show_gantt(self):
         
+        def add_subtask(schedule, t0, d, i_maq, i_tarea):
+            # Dict of a subtask
+            subtask = {'t0': t0, 'd': d, 'i_maq': i_maq, 'i_tarea': i_tarea}
 
-
-
-    def calculate_makespan(self):
-        return 20
+            # Add to schedule
+            schedule.append(subtask)
+        
+        schedule = []
 
 
 
