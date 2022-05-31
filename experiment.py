c7d55defbb5ec50697f4894d61a7c9fa54179b25
kubaunold <jakub@unold.pl>
2022-05-31 17:47:25 +0200

    Creating a graph for mean from iterations. Now time to plot best permutation

--- experiment.py
+++ experiment.py
@@ -1,8 +1,13 @@
 import time
 from genetic import GeneticFlowShop
-from util import read_operations
+from util import read_operations, average_of_list
+import matplotlib.pyplot as plt
+import os
 
 def main():
+    
+    n_iter = 20
+    
     # Number of population
     n_pop = 4
     # Probability of crossover
@@ -21,7 +26,7 @@ def main():
     best_makespans = []
     worst_makespans = []
     average_makespans = []
-    for i in range(3):
+    for i in range(n_iter):
         
         # Run single iteration
         gfs = GeneticFlowShop(  m, n, operation_times, 
@@ -37,12 +42,40 @@ def main():
         worst_makespans.append(gfs.worst_specimen)
         average_makespans.append(gfs.average_specimen)
 
-    x = 5
 
     # average of specific makespans for each epoch of every iteration
-    best_makespans = []
-    worst_makespans = []
-    average_makespans = []    
+    if len(best_makespans) == len(worst_makespans) == len(average_makespans) == n_iter:
+        avg_of_best_makespans = average_of_list(best_makespans)
+        avg_of_worst_makespans = average_of_list(worst_makespans)
+        avg_of_average_makespans = average_of_list(average_makespans)
+        
+        fig, ax = plt.subplots()
+        ax.set(xlabel='Generation', ylabel='Makespan',
+            title=f'Average values of makespans for {n_iter} iterations of Genetic Flow Shop algorithm')
+        
+        ax.plot(range(0, n_epoch), best_makespans[i], label="best makespan")
+        ax.plot(range(0, n_epoch), worst_makespans[i], label="worst makespan")
+        ax.plot(range(0, n_epoch), average_makespans[i], label="average makespan")
+        plt.legend()
+        ax.grid()
+
+
+        plt.draw()
+        plt.pause(0.5)
+        plt.show()
+
+        if not os.path.exists("./results/"):
+            os.mkdir("./results/")
+            
+        fig.savefig("./results/test.png")
+
+        
+
+
+    else:
+        raise Exception("Sorry, length of best, worst and avergae makespans list do not match n_epoch")
+
+
 
 if __name__ == '__main__':
     main()
--- util.py
+++ util.py
@@ -11,3 +11,25 @@ def read_operations(m, n):
 
 
 def silnia(n): return n*silnia(n-1) if n > 1 else 1
+
+
+def average_of_list(m: list[list[int]]) -> list[int]:
+    """From 2D list creates 1D list, where cell is an average value of whole column"""
+        
+    avg_list = [0] * len(m[0])
+
+    # calculate sum
+    for c in range(len(m[0])):
+        # for each column
+        for r in range(len(m)):
+            # for each row
+            avg_list[c] += m[r][c]
+    
+    # calculate average
+    avg_list = [x/len(m) for x in avg_list]
+
+    return avg_list
+
+if __name__ == '__main__':
+    m = [[1,2,3,4, 7], [5,6,7,8, 11]]
+    print(average_of_list(m))
